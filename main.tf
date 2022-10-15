ed6116af8bc84a3b1426128aaf858968b1e319e0
Kenneth Yeung <ken.young887@gmail.com>
2022-10-15 21:08:38 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,94 @@
+terraform {
+    backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "12345terraformstate12345"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-north-1"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-state"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-north-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0bcf2639b551f6b31"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_kms_key" "terraform-bucket-key" {
+ description             = "This key is used to encrypt bucket objects"
+ deletion_window_in_days = 10
+ enable_key_rotation     = true
+}
+
+resource "aws_kms_alias" "key-alias" {
+ name          = "alias/terraform-bucket-key"
+ target_key_id = aws_kms_key.terraform-bucket-key.key_id
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+ bucket = "12345terraformstate12345"
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.terraform-state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.terraform-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.terraform-state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
+       sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+ bucket = aws_s3_bucket.terraform-state.id
+
+ block_public_acls       = true
+ block_public_policy     = true
+ ignore_public_acls      = true
+ restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform-state" {
+ name           = "terraform-state"
+ read_capacity  = 20
+ write_capacity = 20
+ hash_key       = "LockID"
+
+ attribute {
+   name = "LockID"
+   type = "S"
+ }
+}
