4225601967bf13d92309b3206e4409d000e63356
Kenneth Yeung <ken.young887@gmail.com>
2022-10-17 10:02:31 +0200

    github actions

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    backend "s3" {
+  backend "s3" {
     # Replace this with your bucket name!
     bucket         = "12345terraformstate12345"
     key            = "global/s3/terraform.tfstate"
